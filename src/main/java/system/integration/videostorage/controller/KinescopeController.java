fb257203d90efc1f598e33dfb86d5c79c38344ac
Nikita777-coder <61533809+Nikita777-coder@users.noreply.github.com>
2025-03-30 17:18:11 +0300

    make united structure

--- src/main/java/system/integration/videostorage/controller/KinescopeController.java
+++ src/main/java/system/integration/videostorage/controller/KinescopeController.java
@@ -3,6 +3,7 @@ package system.integration.videostorage.controller;
 import jakarta.validation.Valid;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.multipart.MultipartFile;
 import reactor.core.publisher.Mono;
 import system.integration.videostorage.dto.VideoStorageUploadRequest;
 import system.integration.videostorage.dto.VideoStorageUploadResponse;
@@ -22,11 +23,31 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class KinescopeController {
     private final KinescopeService kinescopeService;
-    @PostMapping
+    @PostMapping("/by-url")
     @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
-    public VideoStorageUploadResponse upload(@Valid @RequestBody VideoStorageUploadRequest kinescopeUploadRequest) {
+    public VideoStorageUploadResponse uploadByUrl(@Valid @RequestBody VideoStorageUploadRequest kinescopeUploadRequest) {
         var ans = kinescopeService.upload(kinescopeUploadRequest);
+        return ans;
+    }
+
+    @PostMapping(
+            value = "/by-upload-video",
+            consumes = "multipart/form-data"
+    )
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public VideoStorageUploadResponse uploadByLocalVideo(@RequestParam String title,
+                                                         @RequestParam("upload-video") MultipartFile file,
+                                                         @RequestParam(required = false) String description) {
+        var ans = kinescopeService.upload(
+                VideoStorageUploadRequest.builder()
+                        .title(title)
+                        .uploadVideo(file)
+                        .description(description)
+                        .build()
+        );
+
         return ans;
     }
 
